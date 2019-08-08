85e017e3f4d5a03b69291d4c209d4d4f2f25c35d
marvintherain <daniel.markow@posteo.de>
2019-08-08 22:46:55 +0200

    added struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::time::{SystemTime, Duration};
+use std::path::PathBuf;
+
+use walkdir::WalkDir;
+
+#[derive(Debug)]
+struct File {
+    path: PathBuf,
+    creation_date: SystemTime,
+    time_since_creation: Duration,
+}
+
 fn main() {
     println!("Hello, world!");
 }
