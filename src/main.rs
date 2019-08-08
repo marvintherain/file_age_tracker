652e6832603ece3b540462eb7cd1a4bf81ebd5c2
marvintherain <daniel.markow@posteo.de>
2019-08-08 22:57:28 +0200

    added struct vector gen

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::time::{SystemTime, Duration};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use walkdir::WalkDir;
 
@@ -10,6 +10,37 @@ struct File {
     time_since_creation: Duration,
 }
 
+impl File {
+    fn new(path: PathBuf, creation_date: SystemTime) -> File {
+        File {
+            path,
+            creation_date,
+            time_since_creation: creation_date.elapsed().unwrap(),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    //to-do: tage path as command line param
+    let dir: &Path = Path::new("C://users//danie//Desktop//testdir//");
+    
+    let walker = WalkDir::new(dir);
+    let mut entries: Vec<File> = vec![];
+    for entry in walker {
+        let entry = entry.unwrap();
+        if entry.metadata().unwrap().is_dir() == false {
+            let my_file = File::new(entry
+                                        .path()
+                                        .to_path_buf(), 
+                                    entry
+                                        .metadata()
+                                        .unwrap()
+                                        .created()
+                                        .unwrap()
+                                    );
+            entries.push(my_file);
+        }
+    }
+
+    println!("{:?}", entries);
 }
