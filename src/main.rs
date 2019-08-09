b19876fd3ca0e4e8b2e0f2b770002f5ee20e0e6f
marvintherain <daniel.markow@posteo.de>
2019-08-09 14:30:33 +0200

    added table formatting in cli

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use std::fs;
 
 use walkdir::WalkDir;
 
+#[macro_use] extern crate prettytable;
+use prettytable::{Table, Row, Cell};
+
 #[derive(Debug)]
 struct File {
     path: PathBuf,
@@ -21,25 +24,41 @@ impl File {
     }
 }
 
+//--------------------------------------------------
+
 fn main() {
     //to-do: take path as command line param
     let dir: &Path = Path::new("C://users//danie//Desktop//testdir//");
     
     let entries = read_folder_content(dir);
 
-    println!("{:?}\n", entries);
-
     //to-do: except comparison param from command line
     let compare_param = Duration::new(60000, 0);
 
+    let filtered_entries = filter_files(entries, compare_param);
+    
+    let mut table = Table::new();
+
+    println!("\nfiles that exceed creation date parameter");
+    table.add_row(row!["path", "filename"]);
+    for entry in &filtered_entries {
+        table.add_row(row![entry.path.parent().unwrap().to_str().unwrap(),
+            entry.path.file_name().unwrap().to_str().unwrap()
+        ]);
+    }
+    table.printstd();
+    // fs::remove_file(&filtered_entries[0].path).expect("Could not delete");
+}
+
+//--------------------------------------------------
+
+fn filter_files(entries: Vec<File>, compare_param: Duration) -> Vec<File> {
     let filtered_entries: Vec<File> = entries
         .into_iter()
         .filter(|entry| entry.time_since_creation >= compare_param)
         .collect();
-
-    println!("{:?}\n", filtered_entries);
-
-    // fs::remove_file(&filtered_entries[0].path).expect("Could not delete");
+    
+    filtered_entries
 }
 
 fn read_folder_content(path: &Path) -> Vec<File> {
